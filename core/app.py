c3fd1a651cec4d61ad6ba145f7cb9cff0a48d2b4
Mohapatra <pankaj.mest@gmail.com>
2018-11-05 13:59:05 -0800

    questionnaires

--- core/app.py
+++ core/app.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
+import collections
+import datetime
 
 import requests
 from flask import Flask, request, Response
+from flask_cors import CORS
 import pprint
 import json
 
@@ -17,6 +20,21 @@ config = file_config()
 log = logger.getLogger(__name__)
 
 application = Flask(__name__)
+CORS(application)
+
+HEADER = {'Access-Control-Allow-Origin': '*'}
+
+def pretty_print_POST(req):
+    """
+    This method takes a request and print
+    """
+    log.info('{}\n{}\n{}\n\n{}'.format(
+        '-----------START-----------',
+        req.method + ' ' + req.url,
+        '\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
+        '\n'.join('{}: {}'.format(k, v) for k, v in req.args.to_dict().items()),
+    ))
+
 
 @application.route('/', methods=['GET'])
 def verify():
@@ -50,7 +68,7 @@ def get_metric(*args, **kwargs):
         send_data["values"].append(temp)
     log.info("sent response \n{}".format(pprint.pformat(send_data)))
 
-    return Response(json.dumps(send_data), status=200, mimetype='application/json')
+    return Response(json.dumps(send_data), headers=HEADER, status=200, mimetype='application/json')
 
 
 @application.route('/get_max_metric', methods=['GET'])
@@ -79,20 +97,35 @@ def get_max_metric(*args, **kwargs):
         send_data["values"].append(temp)
     log.info("sent response \n{}".format(pprint.pformat(send_data)))
 
-    return Response(json.dumps(send_data), status=200, mimetype='application/json')
+    return Response(json.dumps(send_data), headers=HEADER, status=200, mimetype='application/json')
 
 
+@application.route('/get_questions', methods=['GET'])
+def get_questions():
+    statement = query.get_questions
+    result = db_fetch(statement)
+    send_data = []
+    for r in result:
+        send_data.append(collections.OrderedDict({u"q_id": r[0], u"qst": r[1]}))
 
-def pretty_print_POST(req):
-    """
-    This method takes a request and print
-    """
-    log.info('{}\n{}\n{}\n\n{}'.format(
-        '-----------START-----------',
-        req.method + ' ' + req.url,
-        '\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
-        '\n'.join('{}: {}'.format(k, v) for k, v in req.args.to_dict().items()),
-    ))
+    return Response(json.dumps(send_data), headers=HEADER, status=200, mimetype='application/json')
+
+
+@application.route('/save_response', methods=['OPTIONS','POST'])
+def save_response():
+    response = request.json
+    uid = response["user_id"]
+    val = ""
+    for a in response["answers"]:
+        val += ",('{}','{}',{},'{}')".format(uid,a["qid"],a["ans"],str(datetime.datetime.now()).split('.')[0])
+    print query.save_response_ins.format(val[1:])
+    ok = db_insup(query.save_response_ins.format(val[1:]))
+    if ok:
+        token = "ok"
+    else:
+        token = "Error saving"
+
+    return Response(json.dumps({"msg": token}), headers=HEADER, status=200, mimetype='application/json')
 
 
 if __name__ == '__main__':
--- core/query.py
+++ core/query.py
@@ -6,3 +6,7 @@ and event_timestamp::date between '{}' and '{}'"
 get_max_metric = "select event_timestamp, max(metric_value) from \
 health_metrics where userid='{}' and metric_label='{}' \
 and event_timestamp::date between '{}' and '{}' group by event_timestamp"
+
+get_questions = "select * from questionnaire"
+
+save_response_ins = "insert into questionnaire_response values {}"
